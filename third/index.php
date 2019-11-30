c882fe99288eddbe5a10255afa6d25b47767470e
Artem Kosulin <toytox@gmail.com>
2019-11-30 22:59:50 +0300

    Отредактировал ДЗ №3.

--- third/index.php
+++ third/index.php
@@ -1,14 +1,30 @@
 <?php
-require ('src/functions.php');
+require('src/functions.php');
 
 echo task1($fileData);
 
+$musicArray = [
+    'Rock' => [
+        'first' => ['band' => 'Queen', 'album' => 'A Night at the Opera', 'topSong' => 'Bohemian Rhapsody'],
+        'second' => ['band' => 'Scorpions', 'album' => 'Crazy World', 'topSong' => 'Wind of Change'],
+        ['band' => 'Metallica', 'album' => 'Metallica', 'topSong' => 'Nothing Else Matters'],
+    ]
+];
+$musicArray2 = [
+    'Rap' => [
+        ['name' => 'Eminem', 'lp' => ' The Marshall Mathers', 'song' => 'Stam'],
+        ['band' => 'Scorpions', 'album' => 'Crazy World', 'topSong' => 'Wind of Change'],
+        ['band' => 'Metallica', 'album' => 'Metallica', 'topSong' => 'Nothing Else Matters'],
+    ]
+];
 echo '<pre>';
 print_r(task2($musicArray, $musicArray2));
 echo '</pre>';
 
+$arrayNumber = [];
 echo task3($arrayNumber) . '<p>';
 
-echo task4 ($sideInfo) . '<p>';
+$sideInfo = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
+echo task4($sideInfo) . '<p>';
 
-echo task5 ($sideInfo);
+echo task5($sideInfo);
--- third/src/functions.php
+++ third/src/functions.php
@@ -29,44 +29,28 @@ function task1($fileData)
 }
 
 //Задача 3.2
-$musicArray = [
-    'Rock' => [
-        'first' => ['band' => 'Queen', 'album' => 'A Night at the Opera', 'topSong' => 'Bohemian Rhapsody'],
-        'second' => ['band' => 'Scorpions', 'album' => 'Crazy World', 'topSong' => 'Wind of Change'],
-        ['band' => 'Metallica', 'album' => 'Metallica', 'topSong' => 'Nothing Else Matters'],
-    ]
-];
-$musicArray2 = [
-    'Rap' => [
-        ['name' => 'Eminem', 'lp' => ' The Marshall Mathers', 'song' => 'Stam'],
-        ['band' => 'Scorpions', 'album' => 'Crazy World', 'topSong' => 'Wind of Change'],
-        ['band' => 'Metallica', 'album' => 'Metallica', 'topSong' => 'Nothing Else Matters'],
-    ]
-];
+
 function task2($musicArray, $musicArray2)
 {
-    file_put_contents('output.json', json_encode($musicArray));
+    $musicString = file_put_contents('output.json', json_encode($musicArray));
     $music = file_get_contents('output.json');
     if (rand(0, 1)) {
-    //изменяем данные
+        //изменяем данные
         file_put_contents('output2.json', json_encode($musicArray2));
     } else {
         file_put_contents('output2.json', $music);
     }
-    $musicOther = file_get_contents('output2.json');
-    $musicString = '{"Rock":[{"band":"Queen","album":"A Night at the Opera","topSong":"Bohemian Rhapsody"},{"band":"Scorpions","album":"Crazy World","topSong":"Wind of Change"},{"band":"Metallica","album":"Metallica","topSong":"Nothing Else Matters"}]}';
-    $musicOtherString = '{"Rock":[{"band":"Eminem","album":"A Night at the Opera","topSong":"Bohemian Rhapsody"},{"band":"Scorpions","album":"Californi","topSong":"Wind of Change"},{"band":"Metallica","album":"Metallica","topSong":"Nothing Else Matters"}]}';
+    $musicOtherString = file_get_contents('output2.json');
+
     json_decode($musicString, true);
     json_decode($musicOtherString, true);
     $result = array_diff_assoc($musicArray['Rock'], $musicArray2['Rap']);
-//    echo '<pre>';
     return ($result);
-//    echo '</pre>';
 }
 
 // Задача 3.3
-$arrayNumber = [];
-function task3 ($arrayNumber)
+
+function task3($arrayNumber)
 {
     for ($number = 0; $number < 50; $number++) {
         $arrayNumber[] = rand(1, 100);
@@ -75,7 +59,6 @@ function task3 ($arrayNumber)
     $newFile = fopen('numbers.csv', 'w');
     fputcsv($newFile, $arrayNumber, ';');
 
-    //echo '<pre>' . print_r($arrayNumber, 1) . '</pre>';
     fclose($newFile);
 
     $getFile = fopen('numbers.csv', 'r');
@@ -91,21 +74,20 @@ function task3 ($arrayNumber)
 }
 
 //Задача 3.4
-$sideInfo = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
 
-function task4 ($sideInfo)
+function task4($sideInfo)
 {
-$decoded = json_decode($sideInfo, true);
-$pageId = $decoded['query']['pages']['15580374']['pageid'];
+    $decoded = json_decode($sideInfo, true);
+    $pageId = $decoded['query']['pages']['15580374']['pageid'];
 
-return ($pageId);
+    return ($pageId);
 }
 
-function task5 ($sideInfo)
+function task5($sideInfo)
 {
-$decoded = json_decode($sideInfo, true);
-$pageTitle = $decoded['query']['pages']['15580374']['title'];
+    $decoded = json_decode($sideInfo, true);
+    $pageTitle = $decoded['query']['pages']['15580374']['title'];
 
-return ($pageTitle);
+    return ($pageTitle);
 }
 
